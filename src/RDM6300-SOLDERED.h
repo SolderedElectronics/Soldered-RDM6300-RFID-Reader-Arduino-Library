8055af01e32695eff9455779ca6086ac0c480e1e
nitko12 <nitko12@hotmail.com>
2021-07-29 11:06:59 +0200

    final

---
+++ src/RDM6300-SOLDERED.h
@@ -0,0 +1,40 @@
+/**
+ **************************************************
+ *
+ * @file        RDM6300-SOLDERED.h
+ * @brief       Main header for RDM6300 Library
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     Zvonimir Haramustek for soldered.com
+ ***************************************************/
+
+#ifndef __RDM6300_SOLDERED__
+#define __RDM6300_SOLDERED__
+
+#include "Arduino.h"
+
+#include "libs/rdm6300/src/rdm6300.h"
+#include <SoftwareSerial.h>
+
+class RDM6300 : public Rdm6300
+{
+  public:
+    RDM6300(int rx_pin = 8) : Rdm6300()
+    {
+        ss = new SoftwareSerial(rx_pin, -1);
+    }
+
+    void begin()
+    {
+        ss->begin(RDM6300_BAUDRATE);
+        Rdm6300::begin(ss);
+    }
+
+  private:
+    SoftwareSerial *ss;
+};
+
+#undef Rdm6300
+
+#endif
